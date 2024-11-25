3b0e424e646adffd69d4c0a40424bbf4144ace53
Katlesty <yahaira.chavez@tecsup.edu.pe>
2024-11-24 21:03:12 -0500

    Initial Commit

---
+++ Actividad13_Chavez&Antezano/Actividad13_Chavez_AntezanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Actividad13_Chavez_AntezanoApp.swift
+//  Actividad13_Chavez&Antezano
+//
+//  Created by Mac15 on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Actividad13_Chavez_AntezanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Actividad13_Chavez&Antezano/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Actividad13_Chavez&Antezano
+//
+//  Created by Mac15 on 24/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
